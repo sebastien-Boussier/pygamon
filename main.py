acd4967720a66409d7fdc30be878d61f615eebb7
sebastien <seb.boussier@gmail.com>
2021-08-04 11:33:10 +0200

    Ajout game.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import pygame
+from game import Game
+
 pygame.init()
-
-# creer la fenetre du jeu
-pygame.display.set_mode((800,600))
-pygame.display.set_caption("Pygamon - Adventure")
-
-# creer la boucle du jeu
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
+myGame = Game()
+myGame.run()
